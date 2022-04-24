570159a40e6bd1719a878056c8d307a06e329ca9
Mateokruljac <94260904+Mateokruljac@users.noreply.github.com>
2022-04-24 19:50:56 +0200

    Update SERVER.py

--- SERVER.py
+++ SERVER.py
@@ -1,44 +1,36 @@
 import socket
 import threading
 
-HOST = "127.0.0.1" #localhost -> mogli smo ga napisati i direkt u bind, ali meni ovako ljepše izgleda
-PORT = 8005 #ista stvar kao i host
-                               #address family
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#tcp
+HOST = "127.0.0.1" 
+PORT = 8005 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind((HOST,PORT))
 server.listen()
 print("Server is listening...")
 
-# u ove prazne liste dodajemo klijente i njihove nadimke
 clients = []
 nicknames = []
 
-#tko može poslati poruku i komu se emitira? svaki klijent koji je aktivan
 def broadcast (message):
     for client in clients:
         client.send(message)
-#print što se prikazuje na serveru a broadcast štp se emitira ostalim klijentima
-def handle (client):
-    #koliko klijent može unositi poruka? KOLIKO HOĆE!
+ handle (client):
+
     while True:
         try:
-            #ako uspije unijeti, ostali je primaju,dekodiraju
-            message = client.recv(1024).decode("utf-8") #  koliko je velika poruka?
+            message = client.recv(1024).decode("utf-8") 
             broadcast(message)
-        except:# no što ako ipak nije tako? što raditi? moramo iznaciti klijenta?
-            #Ali kako znati kojega izbaciti?
+        except:
             index = clients.index(client)
             clients.remove(index)
             client.close()
             nickname = nicknames.index[index]
             broadcast(f"{nickname} has left the chat!")
-            # zašto broadcast a ne print?
-            # zato što drugim klijentima stigne također poruka da je ovaj napustio server
+           
             nicknames.remove(nickname)
 def receive ():
-    #primiti poruku #koliko dugo...cijelo vrijeme dok se šalje
+    
     while True:
-        #server prihvaća klijente i njihovu addressu
+      
         client, address = server.accept()
         print(f"Connection with address: {address}")
         client.send("NICKNAME".encode("ascii"))
