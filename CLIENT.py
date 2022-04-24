a2f95052adebfbdb007d22ac7d61bfe2040e019b
Mateokruljac <94260904+Mateokruljac@users.noreply.github.com>
2022-04-24 18:25:07 +0200

    Update and rename KLIJENT.py to CLIENT.py

--- CLIENT.py
+++ CLIENT.py
@@ -1,33 +1,31 @@
 import threading
 import socket
 from better_profanity import profanity
-# osnovna građa
+
 HOST = "127.0.0.1"
 PORT = 8005
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((HOST,PORT))
-# klijent mora imati također opciju primanje poruka od drugih korisnika,
-# ali isto tako i unos svoje poruke
-#no prvo bira nadimak
+
 nickname = input("Choose a nickname: ")
 
 def receive():
-    while True: # iz istog razloga kao i kod servera
+    while True: 
       try:
          message = client.recv(1024).decode("utf-8")
-         if message == "NICKNAME": #a prva će poruka sigurno biti nickname 
+         if message == "NICKNAME": 
              client.send(bytes(nickname,"utf-8"))
          else:
               print(message)
       except:
           print("!!!!Error!!!!")
           client.close()
-          break # gotova while loop i više se ništa ne događa
+          break 
           
 def write():
-    #korsinik ispisuje poruku, ali prije poruke pisat će tko šalje
+   
     while True:
-      # user unosi poruku, no ako poruka sadrži psovku, server šalje poruku useru da bude pristojan
+     
       write_message = input()
       censor_msg = profanity.censor(write_message,"*")
       if "*" in write_message:
